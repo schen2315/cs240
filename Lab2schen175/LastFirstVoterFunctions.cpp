43fa2147213af6fe11dd5ff43a8f6dda08bd061a
Song Chen <schen2315@bths.edu>
2016-09-12 15:33:22 -0400

    need voter

--- Lab2schen175/LastFirstVoterFunctions.cpp
+++ Lab2schen175/LastFirstVoterFunctions.cpp
@@ -6,14 +6,24 @@ void New(string &last, string &first, int &age, int &strnum, string &strname,
 	string &town, string &zip, float &donated){
 	donated = 0;
 	cout << "Last Name:" << endl;
-	cin >> last;
 	cout << "First Name" << endl;
-	cin >> first;
+	cout << "Age:" << endl;
+	cout << "Street Name:" << endl;
+	cout << "Street Number:" << endl;
+	cout << "Town:" << endl;
+	cin >> last >> first >> age >> strname >> strnum >> town >> zip;
+	donated = 0;
 };
 
 void Update(string &last, string &first, int &age, int &strnum, string &strname,
 	string &town, string &zip, float &donated){
-
+	cout << "Last Name:" << endl;
+	cout << "First Name" << endl;
+	cout << "Age:" << endl;
+	cout << "Street Name:" << endl;
+	cout << "Street Number:" << endl;
+	cout << "Town:" << endl;
+	cin >> last >> first >> age >> strname >> strnum >> town >> zip;
 };
 
 void View(string &last, string &first, int &age, int &strnum, string &strname,
